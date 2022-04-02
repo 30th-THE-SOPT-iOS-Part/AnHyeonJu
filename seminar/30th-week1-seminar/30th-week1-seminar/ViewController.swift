46cb1a8916e491513a210748fe8cd51126bc7108
hellozo0 <ahju0011@gmail.com>
2022-04-02 15:57:21 +0900

    ➕[ADD] :  프로젝트 생성 (#1)

---
+++ seminar/30th-week1-seminar/30th-week1-seminar/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  30th-week1-seminar
+//
+//  Created by 안현주 on 2022/04/02.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var messageLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setUI()
+  }
+  
+  func setUI(){
+    messageLabel.text = "행복해.. 선배덜1..?"
+  }
+  
+  @IBAction func doneButtonDidTap(_ sender: Any) {
+    messageLabel.text = "은주야 사랑행"
+    messageLabel.textColor = .green
+    messageLabel.sizeToFit()
+    AudioServicesPlayAlertSound(SystemSoundID(1307))
+  }
+  
+  
+  
+}
+
