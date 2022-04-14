9ef3e4630b2b742ce55f11148169ae7710bc5694
hellozo0 <ahju0011@gmail.com>
2022-04-15 03:58:58 +0900

    ➕[ADD] : 도전과제 탭바 커스텀 (#8)

---
+++ 30th_Instagram/30th_Instagram/Screen/TabBar/RedVC.swift
@@ -0,0 +1,34 @@
+//
+//  RedVC.swift
+//  30th_Instagram
+//
+//  Created by 안현주 on 2022/04/15.
+//
+
+import UIKit
+
+class RedVC: UIViewController {
+
+  // MARK: - Vars & Lets Part
+
+
+  // MARK: - UI Component Part
+
+  
+  // MARK: - Life Cycle Part
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         
+    }
+    
+  // MARK: - IBAction Part
+
+  
+  // MARK: - Custom Method Part
+
+  
+  // MARK: - @objc Function Part
+
+}
+// MARK: - Extension Part
