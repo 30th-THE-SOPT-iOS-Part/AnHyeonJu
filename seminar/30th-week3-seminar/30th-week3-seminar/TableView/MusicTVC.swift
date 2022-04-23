d5a08de496a04d1f78daa009d581bfcf33264bf3
hellozo0 <ahju0011@gmail.com>
2022-04-23 16:12:01 +0900

    [CHORE] 테이블 뷰 구현 완료

--- seminar/30th-week3-seminar/30th-week3-seminar/TableView/MusicTVC.swift
+++ seminar/30th-week3-seminar/30th-week3-seminar/TableView/MusicTVC.swift
@@ -8,16 +8,24 @@
 import UIKit
 
 class MusicTVC: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
+  
+  //Cell을 구분하기 위한 IDentifier
+  static let identifier = "MusicTVC"
+  
+  @IBOutlet weak var albumCoverImage: UIImageView!
+  @IBOutlet weak var musicTitleLabel: UILabel!
+  @IBOutlet weak var musicDescriptionLabel: UILabel!
+  @IBOutlet weak var moveBtn: UIImageView!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  //각 cell 별로 다른 정보가 표시되어야하므로, 값을 넣어주는 함수를 새성
+  func setData(_ musicData: MusicDataModel) {
+    albumCoverImage.image = musicData.albumImgage
+    musicTitleLabel.text = musicData.musicTitle
+    musicDescriptionLabel.text = musicData.description
+  }
+  
 }
--- seminar/30th-week3-seminar/30th-week3-seminar/TableView/TableViewPracticeVC.swift
+++ seminar/30th-week3-seminar/30th-week3-seminar/TableView/TableViewPracticeVC.swift
@@ -8,24 +8,41 @@
 import UIKit
 
 class TableViewPracticeVC: UIViewController {
-
+  
   @IBOutlet weak var tableView: UITableView!
   
   override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    super.viewDidLoad()
+    registerXib()
+  }
+  
+  func registerXib() {
+    let nib = UINib(nibName: MusicTVC.identifier, bundle: nil)
+    tableView.register(nib, forCellReuseIdentifier: MusicTVC.identifier)
     
+    tableView.delegate = self
+    tableView.dataSource = self
+  }
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+extension TableViewPracticeVC: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 90
+  }
+}
 
+extension TableViewPracticeVC: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return MusicDataModel.sampleData.count
+  }
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: MusicTVC.identifier, for: indexPath) as? MusicTVC else { return UITableViewCell()}
+    
+    cell.setData(MusicDataModel.sampleData[indexPath.row])
+    
+    return cell
+  }
+  
+  
 }
